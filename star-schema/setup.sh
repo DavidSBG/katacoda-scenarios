ae8bfd141e89a54d64c3c85ca6938cddf4e5a04b
DavidSBG <david.schoenberger.00@gmail.com>
2021-10-10 17:07:31 +0200

    script update

--- star-schema/setup.sh
+++ star-schema/setup.sh
@@ -1,6 +1,4 @@
 docker run --name postgres -e POSTGRES_PASSWORD=mysecretpassword -d postgres
 docker exec -it postgres bash
-until [ "`docker inspect -f {{.State.Running}} postgres`"=="true" ]; do
-    sleep 0.1;
-done;
+sleep 5
 psql -U postgres
