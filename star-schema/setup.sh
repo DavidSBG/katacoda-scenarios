5d4994443741e5feac500ffa98cdf5fd88eee103
DavidSBG <david.schoenberger.00@gmail.com>
2021-10-10 16:59:41 +0200

    script update

--- star-schema/setup.sh
+++ star-schema/setup.sh
@@ -1,3 +1,4 @@
-docker run --name some-postgres -e POSTGRES_PASSWORD=mysecretpassword -d postgres
+docker run --name postgres -e POSTGRES_PASSWORD=mysecretpassword -d postgres
+docker exec -it postgres bash
 #docker run -d -p 5432:5432 -e POSTGRES_PASSWORD=postgres --name columnarpostgresql abuckenhofer/columnarpostgresql:v1.3
 #docker exec -it columnarpostgresql bash
