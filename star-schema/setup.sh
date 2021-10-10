8fb31070d5b19a737765d96d66309831820004f6
DavidSBG <david.schoenberger.00@gmail.com>
2021-10-10 17:37:27 +0200

    script update

--- star-schema/setup.sh
+++ star-schema/setup.sh
@@ -1,2 +1,2 @@
-docker run --name starschema -e POSTGRES_PASSWORD=postgres -d -p p 5432:5432 postgres
+docker run --name starschema -e POSTGRES_PASSWORD=postgres -d -p 5432:5432 postgres
 docker exec -it starschema bash
